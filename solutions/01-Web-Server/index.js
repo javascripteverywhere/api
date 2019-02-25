f5e70ccbc365cd441babe856c587d0535b34b758
Adam Scott <ascott1@gmail.com>
2019-02-25 05:43:11 -0500

    add port variable

--- solutions/01-Web-Server/index.js
+++ solutions/01-Web-Server/index.js
@@ -1,6 +1,10 @@
 const express = require('express');
+
 const app = express();
+const port = process.env.PORT || 4000;
 
 app.get('/', (req, res) => res.send('Hello World!!!'));
 
-app.listen(4000, () => console.log('Listening on port 4000!'));
+app.listen(port, () =>
+  console.log(`Server running at http://localhost:${port}`)
+);
