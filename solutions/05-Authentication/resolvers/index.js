259cb9497051a9f64e81a83db7e862460f964a3a
Adam Scott <ascott1@gmail.com>
2019-04-07 09:45:14 -0400

    move resolver code to next chapter

--- solutions/05-Authentication/resolvers/index.js
+++ solutions/05-Authentication/resolvers/index.js
@@ -1,13 +1,9 @@
 const Query = require('./query');
 const Mutation = require('./mutation');
-const Note = require('./note');
-const User = require('./user');
 const { GraphQLDateTime } = require('graphql-iso-date');
 
 module.exports = {
   Query,
   Mutation,
-  Note,
-  User,
   DateTime: GraphQLDateTime
 };
