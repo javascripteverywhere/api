f0d1fa371d7eb2697b016d87ec24131ca2c54ac2
bearinawolfpack <bearinawolfpack@gmail.com>
2020-10-06 14:36:34 -0400

    Build GraphQL API, using the apollo-server-express module in index.js

--- src/index.js
+++ src/index.js
@@ -12,15 +12,18 @@ let notes = [
 
 //Construct a schema, using GraphQL schema language
 const typeDefs = gql`
+  type Note {
+    id: ID!
+    content: String!
+    author: String!
+  }
   type Query {
     hello: String!
     notes: [Note!]!
     note(id: ID!): Note!
   }
-  type Note {
-    id: ID!
-    content: String!
-    author: String!
+  type Mutation {
+    newNote(content: String!): Note!
   }
 `;
 
@@ -31,6 +34,17 @@ const resolvers = {
     notes: () => notes,
     note: (parent, args) => {
       return notes.find(note => note.id === args.id);
+    }
+  },
+  Mutation: {
+    newNote: (parent, args) => {
+      let noteValue = {
+        id: String(notes.length + 1),
+        content: args.content,
+        author: 'Adam Scott'
+      };
+      notes.push(noteValue);
+      return noteValue;
     }
   }
 };
