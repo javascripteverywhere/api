34af0573b2eb3452c25418dbc0d3936db8908c8e
bearinawolfpack <bearinawolfpack@gmail.com>
2020-10-06 14:03:36 -0400

    Add Index and Show functionality to initial api in index.js

--- src/index.js
+++ src/index.js
@@ -4,17 +4,34 @@ const { ApolloServer, gql } = require('apollo-server-express');
 // Run the server on a port specified in our .env file or port 4000
 const port = process.env.PORT || 4000;
 
+let notes = [
+  { id: '1', content: 'This is a note', author: 'Adam Scott' },
+  { id: '2', content: 'This is another note', author: 'Harlow Everly' },
+  { id: '3', content: 'Oh hey look, another note!', author: 'Riley Harrison' }
+];
+
 //Construct a schema, using GraphQL schema language
 const typeDefs = gql`
-type Query {
-  hello: String
-}
+  type Query {
+    hello: String!
+    notes: [Note!]!
+    note(id: ID!): Note!
+  }
+  type Note {
+    id: ID!
+    content: String!
+    author: String!
+  }
 `;
 
 // Provide resolver functions to our schema fields
 const resolvers = {
   Query: {
-    hello: () => 'Hello World!'
+    hello: () => 'Hello World!',
+    notes: () => notes,
+    note: (parent, args) => {
+      return notes.find(note => note.id === args.id);
+    }
   }
 };
 
@@ -33,9 +50,3 @@ app.listen({ port }, () =>
 );
 
 // http://localhost:4000/api -> brings you to GraphQL playground.
-
-
-
-
-
-
