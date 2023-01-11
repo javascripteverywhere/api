4bd809a28bb08e57e51ea20805d6de17da4f0b69
SURMACH SIARHEI <SATURN6862909@GMAIL.COM>
2023-01-11 18:49:12 +0300

    test

--- src/index.js
+++ src/index.js
@@ -1,2 +1,6 @@
 // index.js
 // This is the main entry point of our application
+const express = require ('express');
+const app = express();
+app.get ('/', (req,res) => res.send ('Hello World'));
+app.listen (4000, () => console.log ('Listening on port 4000!'));
