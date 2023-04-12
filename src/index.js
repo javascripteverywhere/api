12b3a3e95bfe09faac82e9590e906fc7d0a8aa24
0131ryu <131ryuji@gmail.com>
2023-04-12 17:00:44 +0900

    graphql 설정 및 예제

--- src/index.js
+++ src/index.js
@@ -1,2 +1,60 @@
-// index.js
-// This is the main entry point of our application
+const { ApolloServer, gql } = require('apollo-server-express');
+const express = require('express');
+const app = express();
+
+const port = process.env.PORT || 4000;
+
+let notes = [
+  { id: '1', content: 'This is a note', author: 'Adam Scott' },
+  { id: '2', content: 'This is anothter note', author: 'Harlow Everly' },
+  { id: '3', content: 'Oh hey look, another note!', author: 'Rilley Harrison' }
+];
+
+const typeDefs = gql`
+  type Note {
+    id: ID!
+    content: String!
+    author: String!
+  }
+  type Query {
+    hello: String
+    notes: [Note!]!
+    note(id: ID!): Note!
+  }
+  type Mutation {
+    newNote(content: String!): Note!
+  }
+`;
+
+const resolvers = {
+  Query: {
+    hello: () => 'Hello world',
+    notes: () => notes,
+    note: (parent, args) => {
+      return notes.find(note => note.id === args.id);
+    }
+  },
+  Mutation: {
+    newNote: (parent, args) => {
+      let noteValue = {
+        id: String(notes.length + 1),
+        content: args.content,
+        author: 'Adam Scott'
+      };
+      notes.push(noteValue);
+      return noteValue;
+    }
+  }
+};
+
+//아폴로 서버 설정
+const server = new ApolloServer({ typeDefs, resolvers });
+
+//아폴로 그래프QL 미들웨어를 적용, 경로를 /api로 설정
+server.applyMiddleware({ app, path: '/api' });
+
+app.listen({ port }, () => {
+  console.log(
+    `GraphQL Server running at http://localhost:${port}${server.graphqlPath}`
+  );
+});
