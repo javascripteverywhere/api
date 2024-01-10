dff27be21cd169cd3638af3d9cf716de75cc5f2a
Blossom Dugbatey <41967164+BlossomDugbatey@users.noreply.github.com>
2024-01-10 14:02:26 +0000

    apollo server setup

--- src/index.js
+++ src/index.js
@@ -1,7 +1,33 @@
 const express = require('express');
+const { ApolloServer, gql } = require('apollo-server-express');
+
 const app = express();
 
+//run server on port in .env file or use 4000
 const port = process.env.PORT || 4000;
 
-app.get('/', (req, res) => res.send('Hello Web Server!!!'));
-app.listen(port, () => console.log(`Server running on http://localhost:${port}`));
+//define schema using GraphQL schema language
+const typeDefs = gql`
+  type Query {
+    hello: String
+  }
+`;
+
+//resolver function for schema fields
+const resolvers = {
+  Query: {
+    hello: () => 'Hello World!'
+  }
+};
+
+//setup apollo server
+const server = new ApolloServer({ typeDefs, resolvers });
+//apply apollo graphql middleware and set path to /api
+server.applyMiddleware({ app, path: '/api' });
+
+app.listen({ port }, () =>
+  console.log(
+    `GraphQL Server running at http://localhost:${port}${server.graphqlPath}`
+  )
+);
+
