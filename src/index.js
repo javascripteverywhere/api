a0f601b79672d051d485b5c857daf326e2da1daa
Blossom Dugbatey <41967164+BlossomDugbatey@users.noreply.github.com>
2024-01-10 13:24:01 +0000

    server setup

--- src/index.js
+++ src/index.js
@@ -1,2 +1,7 @@
-// index.js
-// This is the main entry point of our application
+const express = require('express');
+const app = express();
+
+const port = process.env.PORT || 4000;
+
+app.get('/', (req, res) => res.send('Hello Web Server!!!'));
+app.listen(port, () => console.log(`Server running on http://localhost:${port}`));
