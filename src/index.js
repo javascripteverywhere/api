36775f40e5cdaff8f1bfd481255eb580d2c76d15
Blossom Dugbatey <41967164+BlossomDugbatey@users.noreply.github.com>
2024-01-10 23:14:07 +0000

    query and mutation

--- src/index.js
+++ src/index.js
@@ -6,6 +6,12 @@ const app = express();
 //run server on port in .env file or use 4000
 const port = process.env.PORT || 4000;
 
+let notes = [
+    { id: '1', content: 'This is a note', author: 'Adam Scott' },
+    { id: '2', content: 'This is another note', author: 'Harlow Everly' },
+    { id: '3', content: 'Oh hey look, another note!', author: 'Riley Harrison' }
+];
+
 //define schema using GraphQL schema language
 const typeDefs = gql`
  type Note {
@@ -19,6 +25,10 @@ const typeDefs = gql`
     notes: [Note!]!
     note(id: ID!): Note!
   }
+
+  type Mutation {
+    newNote(content: String!): Note!
+}
 `;
 
 //resolver function for schema fields
@@ -28,6 +38,17 @@ const resolvers = {
     notes: () => notes,
     note: (parent, args) => {
         return notes.find(note => note.id === args.id);
+    }
+  },
+  Mutation: {
+    newNote: (parent, args) => {
+      let noteValue = {
+        id: String(notes.length + 1),
+        content: args.content,
+        author: 'Adam Scott'
+      };
+      notes.push(noteValue);
+      return noteValue;
     }
   }
 };
@@ -43,8 +64,3 @@ app.listen({ port }, () =>
   )
 );
 
-let notes = [
-    { id: '1', content: 'This is a note', author: 'Adam Scott' },
-    { id: '2', content: 'This is another note', author: 'Harlow Everly' },
-    { id: '3', content: 'Oh hey look, another note!', author: 'Riley Harrison' }
-];
